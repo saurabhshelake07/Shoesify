f29eaa72d1cb753d5c814c90a3437975127a782e
saurabhshelake07 <saurabhshelake5051@gmail.com>
2025-08-18 15:00:14 +0530

    Database Model Created

--- ShoeSify/ShoeSApp/models.py
+++ ShoeSify/ShoeSApp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class ShoeModel(models.Model):
+    name = models.CharField(max_length=60)
+    brand = models.CharField(max_length=60)
+    category = models.CharField(max_length=60)
+    color = models.CharField(max_length=60)
+    gender = models.CharField(max_length=60)
+    size = models.IntegerField()
+    stock = models.IntegerField()
+    price = models.FloatField()
